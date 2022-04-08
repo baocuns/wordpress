aff8efb24a6a233d7730fd0b59c500e8024b3db1
bao-rin <baonv.23.student@fit.tdc.edu.vn>
2022-04-08 08:38:16 +0700

    update

---
+++ wp-content/themes/mytheme/footer.php
@@ -0,0 +1,10 @@
+</div>
+<!--end container -->
+<footer>
+<div class="copyright">
+&copy;<?php echo date('Y')?> - <?php bloginfo('sitename') ?>
+</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
