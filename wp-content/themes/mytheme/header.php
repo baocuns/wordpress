57abd36515b605b553ea1f70936a1a9cd77300c9
bao-rin <baonv.23.student@fit.tdc.edu.vn>
2022-04-08 08:25:09 +0700

    push wordpressInitial commitInitial commitpush wordpressInitial commitInitial commit

---
+++ wp-content/themes/mytheme/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" >
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+<!--container-->
+<div id="container">
+<header id="header">
+<?php mytheme_logo(); ?>
+<?php mytheme_menu('primary-menu' ); ?>
+</header>
