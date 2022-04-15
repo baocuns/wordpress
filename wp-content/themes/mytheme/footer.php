0de40152f00fb8df23b4b27e48c0d9328ec6d5a0
baocuns <baonv.23.student@fit.tdc.edu.vn>
2022-04-15 09:00:58 +0700

    update mytheme

---
+++ wp-content/themes/mytheme/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <div class="copyright">
+            &copy;<?php echo date('Y') ?> - <?php bloginfo('sitename') ?>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
